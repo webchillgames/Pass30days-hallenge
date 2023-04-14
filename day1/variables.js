f73ca0304d173d8eff9d195daa483a1db1a99a6b
webchillgames <nanaprettysinger@gmail.com>
2023-04-14 21:20:04 +0800

    day 2

--- day1/variables.js
+++ day1/variables.js
@@ -12,21 +12,20 @@ export const isFrontendDeveloper = true; // Boolean
 // Indonesia`;
 // const age = `33`;
 
-// const firstName = "Artem";
-// const lastName = "Mikhailov";
-// const maritalStatus = "married";
-// const country = "Bali
-// Indonesia";
-// const age = "33";
+const firstName = "Artem";
+const lastName = "Mikhailov";
+const maritalStatus = "married";
+const country = "Bali Indonesia";
+const age = "33";
 
 const myAge = 27;
 const yourAge = 33;
 
-// console.log(
-//   `I am ` +
-//     myAge +
-//     ` years old.
-// You are ` +
-//     yourAge +
-//     ` years old`
-// );
+console.log(
+  `I am ` +
+    myAge +
+    ` years old.
+You are ` +
+    yourAge +
+    ` years old`
+);
--- day2/level3.js
+++ day2/level3.js
@@ -1,8 +1,52 @@
-// console.log('Love is the best thing in this world. Some found their love and some are still looking for their love.'.match(/love/gi).length);
-// console.log('You cannot end a sentence with because because because is a conjunction'.match(/because/).length);
+console.log(
+  "Love is the best thing in this world. Some found their love and some are still looking for their love.".match(
+    /love/gi
+  ).length
+);
+console.log(
+  "You cannot end a sentence with because because because is a conjunction".match(
+    /because/
+  ).length
+);
 
-const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
-const clearedSentence = sentence.match(/[a-z\s]/gi).join(' ')
-console.log(clearedSentence)
+const sentence =
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+const clearedSentence = sentence
+  .match(/[\w\s]/gi)
+  .join("")
+  .split(" ");
 
-const sentence2 = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+const words = {};
+
+clearedSentence.forEach((v) => {
+  let i = 0;
+
+  clearedSentence.forEach((j) => {
+    if (j === v) {
+      i++;
+    }
+  });
+
+  if (!words.hasOwnProperty(v)) {
+    words[v] = i;
+  }
+});
+
+let top = 0;
+let topWord;
+
+for (let key in words) {
+  if (words[key] > top) {
+    top = words[key];
+    topWord = key;
+  }
+}
+
+console.log("the most frequent word is: ", topWord);
+
+const sentence2 =
+  "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
+
+const money = sentence2.match(/\d+\s/gi);
+
+const result = money.reduce((a, c) => Number(a) + Number(c), 0);
